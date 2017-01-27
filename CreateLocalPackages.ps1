093eb4991e2a214ab05562f4ebc9c444c4a84e18
Kevin D. Wolf <kevinw@software-logistics.com>
2017-01-27 16:52:52 -0500

    update nuget publish scripts

--- CreateLocalPackages.ps1
+++ CreateLocalPackages.ps1
@@ -1,15 +1,18 @@
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path
 
+Write-Output $scriptPath
+
 Set-Location $scriptPath
 
-. ./BuildAll.ps1
+#. ./BuildAll.ps1
 
 . ./UpdateNuspecVersion.ps1 -preRelease alpha -major 0 -minor 8
 
 $oldChildren = gci $scriptPath -Recurse *.nupkg
-foreach( $oldChild in $oldChildren)
-{
-	Remove-Item $oldChild -Recurse
+foreach( $oldChild in $oldChildren){
+	if(!($oldChild.DirectoryName -match "packages")){
+		Remove-Item $oldChild -Recurse
+	}
 }
 
 $children = gci $scriptPath -recurse *.nuspec
--- PublishRemotePackages.ps1
+++ PublishRemotePackages.ps1
@@ -7,8 +7,10 @@ $children = gci $scriptpath -recurse *.nupkg
 
 # Add this key via control panel environment variables [NUGETAPIKEY] to be obtained from the LagoVista Nuget Account
 
-foreach( $child in $children)
-{
-	Write-Output "Publishing Output $child.fullName" 
-    nuget push $child.fullName -Source https://www.nuget.org/api/v2/package -ApiKey $env:NUGETAPIKEY
+foreach( $child in $children){
+	Write-Output "Evaluating $child.DirectoryName" 
+	if(!($child.DirectoryName -match "packages")){
+		Write-Output "Publishing Output $child.fullName" 
+		nuget push $child.fullName -Source https://www.nuget.org/api/v2/package -ApiKey $env:NUGETAPIKEY
+	}
 }
